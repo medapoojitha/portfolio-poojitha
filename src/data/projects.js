cff6416390135f953c49013c3656151bea9e20a9
Meda Poojitha <medapoojitha31@gmail.com>
2025-12-26 01:57:44 +0530

    images added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,8 @@
+import emailImg from "../assets/email.png";
+import leafImg from "../assets/leaf-disease.png";
+import objectImg from "../assets/object-detection.png";
+import movieImg from "../assets/movie.png";
+
 export const projects = [
   {
     title: "Spam Email Detection",
@@ -5,8 +10,7 @@ export const projects = [
     description:
       "Uses NLP preprocessing, vectorization, and ML classifiers to detect spam emails.",
     tech: "Python, NLP, Machine Learning",
-        image: "/src/assets/email.png",
-
+    image: emailImg,
     github: "https://github.com/medapoojitha/spam-email",
   },
   {
@@ -15,8 +19,7 @@ export const projects = [
     description:
       "Analyzes academic features using regression and classification models.",
     tech: "Python, ML",
-        image: "/src/assets/leaf-disease.png",
-
+    image: leafImg,
     github: "https://github.com/medapoojitha/student-performance",
   },
   {
@@ -25,9 +28,9 @@ export const projects = [
     description:
       "Classifies plant leaf diseases using CNNs for early diagnosis.",
     tech: "Python, CNN, TensorFlow, OpenCV",
-        image: "/src/assets/leaf-disease.png",
-
-    github: "https://github.com/medapoojitha/Leaf-Disease-Detection-Using-Image-Processing",
+    image: leafImg,
+    github:
+      "https://github.com/medapoojitha/Leaf-Disease-Detection-Using-Image-Processing",
   },
   {
     title: "Object Detection & Visualization",
@@ -35,9 +38,9 @@ export const projects = [
     description:
       "CNN-based system that detects and visualizes objects in images and videos.",
     tech: "Deep Learning, OpenCV",
-        image: "/src/assets/object-detection.png",
-
-    github: "https://github.com/medapoojitha/Object-Detection-Visualization-using-CNN",
+    image: objectImg,
+    github:
+      "https://github.com/medapoojitha/Object-Detection-Visualization-using-CNN",
   },
   {
     title: "Movie Recommendation System",
@@ -45,8 +48,8 @@ export const projects = [
     description:
       "Uses collaborative filtering to recommend movies based on user similarity.",
     tech: "Python, Recommendation Systems",
-        image: "/src/assets/movie.png",
-
-    github: "https://github.com/medapoojitha/Movie-Recommendation-System",
+    image: movieImg,
+    github:
+      "https://github.com/medapoojitha/Movie-Recommendation-System",
   },
 ];
